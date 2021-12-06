00872bb8fb4e9c95932a211b53add736e234ee01
Andrea Gandolfi <andgandolfi@gmail.com>
2021-12-06 15:03:18 +0100

    Prepare for day 07

---
+++ src/xyz/gandolfi/aoc21/day07/Main.java
@@ -0,0 +1,18 @@
+package xyz.gandolfi.aoc21.day07;
+
+import xyz.gandolfi.aoc21.Utils;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> inputLines = Utils.getInputFileLines("day07.txt");
+        assert inputLines != null;
+
+        System.out.print("Day 07a: ");
+        System.out.println();
+
+        System.out.print("Day 07b: ");
+        System.out.println();
+    }
+}
